e94219f0aaf6f658885983e1d5ce1ecdb0df6053
Daniel Braga <danielbraga2121@gmail.com>
2023-02-10 23:01:09 -0300

    bug-fix

---
+++ routes/app.routes.js
@@ -0,0 +1,23 @@
+const categoryController = require("../controllers/categories.controller");
+const productController = require("../controllers/products.controller");
+const userController = require("../controllers/users.controller");
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/category", categoryController.create);
+router.get("/category", categoryController.findAll);
+router.get("/category/:id", categoryController.findOne);
+router.put("/category/:id", categoryController.update);
+router.delete("/category/:id", categoryController.delete);
+
+router.post("/product", productController.create);
+router.get("/product", productController.findAll);
+router.get("/product/:id", productController.findOne);
+router.put("/product/:id", productController.update);
+router.delete("/product/:id", productController.delete);
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+
+module.exports = router;
