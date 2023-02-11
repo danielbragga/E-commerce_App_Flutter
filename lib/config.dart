48aef665eec09d74ce34efbf9d07c1137a4311ae
Daniel Braga <89660062+danielbragga@users.noreply.github.com>
2023-02-10 23:38:08 -0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class Config {
   static const String appName = "E-commerce App";
   static const String apiURL = "10.0.0.5:5000";
-  static const String imageURL = "http://10.0.0.5:5000";
+  static const String imageURL = "http://10.0.0.5:5000"; //OBS: usar o IP Local para funcionar.
   static const String categoryAPI = "api/category";
   static const String productAPI = "api/product";
   static const int pageSize = 10;
